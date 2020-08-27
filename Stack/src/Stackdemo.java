2211e3c9e204c8f14a8035bfa5c1e21d2c259366
LenovaPC <LenovaPC@DESKTOP-9FDNQSH>
2020-08-27 10:51:29 +0530

    my commit

---
+++ Stack/src/Stackdemo.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class Stackdemo {
+
+	public static void main(String[] args) {
+
+		Stack l=new Stack();
+		l.push("A");
+		l.push("B");
+		l.push("C");
+		l.push("D");
+		System.out.println(l);
+		System.out.println(l.search("A"));
+		
+	
+	}
+
+}
